9f3ddf40643623ccd1ed2f8a3b0e9bcd49e2d24f
puresmash <puresmash@gmail.com>
2024-04-13 23:28:24 +0800

    Also adopt typescript on server codebase

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import http from 'http';
-import path from 'path';
 
-const __dirname = path.resolve();
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
